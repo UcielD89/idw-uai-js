d2d1a186247bcb61743fc0eb79522c9f8e97ecd4
UcielD89 <ucielprogramador21@gmail>
2025-11-08 19:14:10 -0300

    [ADD] Ejercicios de Variables y Strings

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,18 @@
+// Variables y operadores
+// a)
+var n1 = 1;
+var n2 = 4;
+var result = n1 + n2;
+// console.log(result);
+
+// b)
+var s1 = "Hola"
+var s2 = "Mundo"
+var concatenate = s1 + " " + s2;
+// console.log(concatenate);
+
+// c)
+var str1 = "Que tal"
+var str2 = "a todos"
+var length = str1.length + str2.length;
+// console.log(length);
